71b14541f22c9cd7c50449b7c176529427197767
R-Aniruddha <aniruddha04@gmail.com>
2020-11-06 23:36:05 +0100

    Add Tests for Kill Feature

--- __tests__/weiClinic.tests.js
+++ __tests__/weiClinic.tests.js
@@ -12,7 +12,7 @@ describe('WeiClinic', () => {
         expect(expectedResult).toEqual(actualResult)
     })
 
-    test('create function',() => {
+    test('Create Function',() => {
         const getNewId = jest.fn().mockReturnValue(1)
        
         const CorticalStack = {
@@ -38,5 +38,43 @@ describe('WeiClinic', () => {
         expect(expectedResult).toEqual(actualResult)
     })
 
+
+
+    test('Kill Envelope with stack', () => {
+        const expectedResult = true
+        //const weiClinic = getClinic()
+        getClinic().envelopes = [ {id: 1, idStack: 2} ]
+        getClinic().stacks = [ {id: 2, idEnvelope: 1} ]
+
+
+        const actualResult = getClinic().killEnvelope(1)
+
+        expect(expectedResult).toBe(actualResult)
+        console.log("RESULT = " + getClinic().envelopes)
+        expect(0).toBe(getClinic().envelopes.length)
+        expect(null).toBe(getClinic().stacks[0].idEnvelope)
+    })
+
+    test('Kill Envelope without stack', () => {
+        const expectedResult = true
+        //const weiClinic = getClinic()
+        getClinic().envelopes = [ {id: 1, idStack: null} ]
+
+        const actualResult = getClinic().killEnvelope(1)
+
+        expect(expectedResult).toBe(actualResult)
+        expect(0).toBe(getClinic().envelopes.length)
+    })
+
+        test('Attempt to Kill nonexisting Envelope', () => {
+
+            const expectedResult = false
+            getClinic().envelopes = []
+
+            const actualResult = getClinic().killEnvelope(1)
+
+            expect(expectedResult).toBe(actualResult)
+
+        })
 
 })
